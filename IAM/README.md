0132cb4de8246d4b1b35be41494c9e7c3e32342d
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-01-23 14:30:30 +0700

    nothing to update

--- IAM/README.md
+++ IAM/README.md
@@ -1,31 +1,16 @@
 # Khởi đầu với AWS Cloud với IAM
 
 ## Overview
-IAM là một trong những chủ đề quan trọng nhất trong AWS. Nó là một trong những chủ đề đầu tiên mà bạn cần phải nắm vững khi bắt đầu với AWS.
 
-Vậy IAM là gì? IAM là viết tắt của Identity and Access Management. Nó là một dịch vụ quản lý quyền truy cập của AWS. Nó cho phép bạn quản lý người dùng và quyền truy cập của họ vào các tài nguyên AWS.
+IAM là một trong những chủ đề quan trọng nhất trong AWS. Nó là một trong dịch vụ đầu tiên mà bạn cần phải nắm vững khi bắt đầu với AWS.
+
+Vậy IAM là gì? IAM là viết tắt của Identity and Access Management. Nó là một dịch vụ quản lý quyền và cho phép bạn quản lý người dùng truy cập vào các tài nguyên AWS. Hiểu đơn giản là khi bạn muốn vào nhà một ai đó thì cần phải được chủ nhà cho phép.
+
+Các thành phần chính trong IAM:
 
 - IAM user: là một thực thể được xác định bởi một tên đăng nhập và mật khẩu. Người dùng này có thể được sử dụng để đăng nhập vào AWS Management Console và truy cập vào các tài nguyên AWS.
-- IAM groups: là một tập hợp các người dùng. Nó cho phép bạn gán các chính sách IAM cho nhiều người dùng cùng một lúc.
-- IAM roles: là một thực thể được xác định bởi một tên và một tập hợp các quyền truy cập. Nó không có bất kỳ thông tin xác thực nào. Nó được sử dụng để cung cấp quyền truy cập cho các dịch vụ AWS khác nhau.
-- IAM policies: là một tập hợp các quyền truy cập được định nghĩa bằng cú pháp JSON. Nó được gán cho người dùng, nhóm hoặc vai trò.
+- IAM groups:
+- IAM roles: Được xác định bởi một tên và một tập hợp các quyền truy cập. Nó không có bất kỳ thông tin xác thực nào và được sử dụng để cung cấp quyền truy cập .
+- IAM policies: là đơn vị nhỏ nhất trong IAM được định nghĩa bằng cú pháp JSON. Nó được gán cho user, group hoặc role.
 
-Khi tạo mới một tài khoản trên AWS, thì tài khoản đó được xem là tài khoản root. Nó được quyền truy cập bất kỳ tài nguyên nào trên AWS.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Khi tạo mới một tài khoản trên AWS, thì tài khoản đó được xem là tài khoản root. Nó được quyền truy cập vào tất cả dịch vụ trên AWS.
--- IAM/iam-cdk/lib/iam-cdk-stack.ts
+++ IAM/iam-cdk/lib/iam-cdk-stack.ts
@@ -1,5 +1,13 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { Effect, Group, Policy, PolicyStatement, User } from 'aws-cdk-lib/aws-iam';
+import {
+  Effect,
+  Group,
+  Policy,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+  User
+} from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class IamCdkStack extends Stack {
@@ -14,10 +22,9 @@ export class IamCdkStack extends Stack {
 
     // ====================================================================================================
     // Create a developer policy
-    const developerPolicy = new Policy(this, 'IamDeveloperPolicy', {
-      policyName: 'developer-policy-test-iam',
+    const developerPolicy = new Policy(this, 'developer-test-policy', {
+      policyName: 'developer-test-policy',
       statements: [
-        // Read access to S3, DynamoDB, Lambda
         new PolicyStatement({
           effect: Effect.ALLOW,
           actions: [
@@ -33,35 +40,34 @@ export class IamCdkStack extends Stack {
       ]
     });
 
-    // Create a developer user
-    const developerUser = new User(this, 'IamDeveloperUser', {
-      userName: 'developer-user-test-iam'
+    // Create developer users
+    const developer1User = new User(this, 'developer1-test-user', {
+      userName: 'developer1-test-user'
     });
-    const developerUser2 = new User(this, 'IamDeveloperUser2', {
-      userName: 'developer-user2-test-iam'
+    const developer2User = new User(this, 'developer2-test-user', {
+      userName: 'developer2-test-user'
     });
-    const developerUser3 = new User(this, 'IamDeveloperUser3', {
-      userName: 'developer-user3-test-iam'
+    const developer3User = new User(this, 'developer3-test-user', {
+      userName: 'developer2-test-user'
     });
 
     // Create a developer group
-    const developerGroup = new Group(this, 'IamDeveloperGroup', {
-      groupName: 'developer-group-test-iam'
+    const developerGroup = new Group(this, 'developer-test-group', {
+      groupName: 'developer-test-group'
     });
     // Attach the developer policy to the developer group
     developerGroup.attachInlinePolicy(developerPolicy);
     // Add the developer user to the developer group
-    developerGroup.addUser(developerUser);
-    developerGroup.addUser(developerUser2);
-    developerGroup.addUser(developerUser3);
+    developerGroup.addUser(developer1User);
+    developerGroup.addUser(developer2User);
+    developerGroup.addUser(developer3User);
     // ====================================================================================================
 
     // ====================================================================================================
     // Create a admin policy
-    const adminPolicy = new Policy(this, 'IamAdminPolicy', {
-      policyName: 'admin-policy-test-iam',
+    const adminPolicy = new Policy(this, 'admin-test-policy', {
+      policyName: 'admin-test-policy',
       statements: [
-        // Full access to S3, DynamoDB, Lambda
         new PolicyStatement({
           effect: Effect.ALLOW,
           actions: ['s3:*', 'dynamodb:*', 'lambda:*'],
@@ -71,21 +77,50 @@ export class IamCdkStack extends Stack {
     });
 
     // Create a admin user
-    const adminUser = new User(this, 'IamAdminUser', {
-      userName: 'admin-user-test-iam'
+    const admin1User = new User(this, 'admin1-test-user', {
+      userName: 'admin1-test-user'
     });
-    const adminUser2 = new User(this, 'IamAdminUser2', {
-      userName: 'admin-user2-test-iam'
+    const admin2User = new User(this, 'admin2-test-user', {
+      userName: 'admin2-test-user'
     });
 
     // Create a admin group
-    const adminGroup = new Group(this, 'IamAdminGroup', {
-      groupName: 'admin-group-test-iam'
+    const adminGroup = new Group(this, 'admin-test-group', {
+      groupName: 'admin-test-group'
     });
     // Attach the admin policy to the admin group
     adminGroup.attachInlinePolicy(adminPolicy);
     // Add the admin user to the admin group
-    adminGroup.addUser(adminUser);
-    adminGroup.addUser(adminUser2);
+    adminGroup.addUser(admin1User);
+    adminGroup.addUser(admin2User);
+
+    // ====================================================================================================
+
+    // Create a Role for Lambda to access S3
+    const lambdaPolicy = new Policy(this, 'lambda-test-policy', {
+      policyName: 'lambda-test-policy',
+      statements: [
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: ['s3:Get*', 's3:List*'],
+          resources: [
+            'arn:aws:s3:::lambda-s3-trigger-demo',
+            'arn:aws:s3:::lambda-s3-trigger-demo/*'
+          ]
+        }),
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
+          resources: ['arn:aws:logs:*:*:*']
+        })
+      ]
+    });
+    const lambdaRole = new Role(this, 'lambda-test-role', {
+      roleName: 'lambda-test-role',
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com')
+    });
+    lambdaRole.attachInlinePolicy(lambdaPolicy);
+
+    // ====================================================================================================
   }
 }
