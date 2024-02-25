417de5895bff2b86052e7f753f5af74961226761
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-02-25 11:55:58 +0700

    Update ec2 basic instance in public vpc

--- EC2/ec2-basic/lib/ec2-basic-stack.ts
+++ EC2/ec2-basic/lib/ec2-basic-stack.ts
@@ -1,11 +1,54 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import {
+  AmazonLinuxGeneration,
+  AmazonLinuxImage,
+  Instance,
+  InstanceClass,
+  InstanceSize,
+  InstanceType,
+  KeyPair,
+  SubnetType,
+  UserData,
+  Vpc,
+} from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class Ec2BasicStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // Create a VPC with default configurations
     const vpc = new Vpc(this, 'workshop-test-vpc');
+
+    // Create a user data script to install Apache HTTP server
+    const userData = UserData.forLinux({ shebang: '#!/bin/bash' });
+    userData.addCommands(
+      'yum install -y httpd',
+      'service httpd start',
+      'echo "<h1>Hello, World from $(hostname -f)!</h1>" > /var/www/html/index.html',
+    );
+
+    // Get key pair for EC2 instances
+    const ec2KeyPair = KeyPair.fromKeyPairName(
+      this,
+      'workshop-test-key-pair',
+      'workshop-test-key-pair',
+    );
+
+    // Create a new EC2 instance in public subnet
+    const workshopServerPublic = new Instance(this, 'workshop-test-server-public', {
+      vpc,
+      instanceName: 'workshop-test-server-public',
+      instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
+      machineImage: new AmazonLinuxImage({ generation: AmazonLinuxGeneration.AMAZON_LINUX_2023 }),
+      vpcSubnets: {
+        subnetType: SubnetType.PUBLIC,
+      },
+      requireImdsv2: true,
+      userData: userData,
+      keyPair: ec2KeyPair,
+      detailedMonitoring: true,
+    });
+    workshopServerPublic.applyRemovalPolicy(RemovalPolicy.DESTROY);
   }
 }
