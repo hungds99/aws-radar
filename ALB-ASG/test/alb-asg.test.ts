3f197c357f3491e626cd02eec9aaa7747eef7b79
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-03-16 11:00:04 +0700

    :tada: 16/03/24 learn about ELB and ASG

---
+++ ALB-ASG/test/alb-asg.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AlbAsg from '../lib/alb-asg-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/alb-asg-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AlbAsg.AlbAsgStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-AWS
+## AWS
+
+For my journey into AWS, I have been using the following resources:
+
+- [AWS Certified Solutions Architect - Associate 2024](https://www.udemy.com/course/aws-certified-solutions-architect-associate-saa-c03)
+- [AWS Certified Developer - Associate 2024](https://www.udemy.com/course/aws-certified-developer-associate-dva-c01)
