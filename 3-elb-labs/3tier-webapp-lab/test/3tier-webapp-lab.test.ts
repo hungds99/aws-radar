1f1debd2b40015e4491c70a06be56e91456e7685
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-04-13 11:07:06 +0700

    elb: rename and init some labs

---
+++ 3-elb-labs/3tier-webapp-lab/test/3tier-webapp-lab.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as 3TierWebappLab from '../lib/3tier-webapp-lab-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/3tier-webapp-lab-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new 3TierWebappLab.3TierWebappLabStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
