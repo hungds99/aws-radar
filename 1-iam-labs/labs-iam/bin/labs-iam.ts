2d5a11851fa0859594ad70bb5ed5b48e413da205
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-04-17 14:10:59 +0700

    chore: add dependency aws solution check package

--- 1-iam-labs/labs-iam/bin/labs-iam.ts
+++ 1-iam-labs/labs-iam/bin/labs-iam.ts
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
-import { App } from 'aws-cdk-lib';
+import { App, Aspects } from 'aws-cdk-lib';
 import 'source-map-support/register';
 import { LabsIamStack } from '../lib/labs-iam-stack';
+import { AwsSolutionsChecks } from 'cdk-nag';
 
 const app = new App();
 new LabsIamStack(app, 'LabsIamStack', {
   description: 'The AWS CDK stack for IAM lab',
 });
+
+Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
