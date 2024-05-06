813d3563a675c94c36a57656dea23b22bf9b8de2
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-05-06 10:48:37 +0700

    feat: add postgresdb instance

--- 4-rds-labs/rds-mysql/lib/rds-mysql-stack.ts
+++ 4-rds-labs/rds-mysql/lib/rds-mysql-stack.ts
@@ -1,10 +1,11 @@
-import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { InstanceClass, InstanceSize, InstanceType, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import {
   Credentials,
   DatabaseInstance,
   DatabaseInstanceEngine,
   MysqlEngineVersion,
+  PostgresEngineVersion,
 } from 'aws-cdk-lib/aws-rds';
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
@@ -16,17 +17,37 @@ export class RdsMysqlStack extends Stack {
     const defaultVpc = Vpc.fromLookup(this, 'DefaultVpc', { isDefault: true });
 
     // Create a new RDS MySQL instance
-    const labMysqlDB = new DatabaseInstance(this, 'LabMysqlDB', {
+    // const labMysqlDB = new DatabaseInstance(this, 'LabMysqlDB', {
+    //   vpc: defaultVpc,
+    //   databaseName: 'labMysqlDB',
+    //   engine: DatabaseInstanceEngine.mysql({
+    //     version: MysqlEngineVersion.VER_8_0_36,
+    //   }),
+    //   instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+    //   vpcSubnets: { subnetType: SubnetType.PUBLIC },
+    //   publiclyAccessible: true,
+    //   deleteAutomatedBackups: true,
+    // });
+    // labMysqlDB.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    // new CfnOutput(this, 'LabMysqlDBEndpoint', {
+    //   value: labMysqlDB.dbInstanceEndpointAddress,
+    // });
+
+    // Create a new RDS PostgreSQL instance
+    const labPostgresDB = new DatabaseInstance(this, 'LabPostgresDB', {
       vpc: defaultVpc,
-      databaseName: 'labMysqlDB',
-      engine: DatabaseInstanceEngine.mysql({
-        version: MysqlEngineVersion.VER_8_0_36,
+      databaseName: 'labPostgresDB',
+      engine: DatabaseInstanceEngine.postgres({
+        version: PostgresEngineVersion.VER_16_2,
       }),
       instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
       vpcSubnets: { subnetType: SubnetType.PUBLIC },
       publiclyAccessible: true,
       deleteAutomatedBackups: true,
     });
-    labMysqlDB.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    labPostgresDB.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    new CfnOutput(this, 'LabPostgresDBEndpoint', {
+      value: labPostgresDB.dbInstanceEndpointAddress,
+    });
   }
 }
