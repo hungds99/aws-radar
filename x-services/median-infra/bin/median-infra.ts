7db4af16ec99850793e436f054be8a43ca86066b
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-05-13 11:34:40 +0700

    add rds configuration

--- x-services/median-infra/bin/median-infra.ts
+++ x-services/median-infra/bin/median-infra.ts
@@ -3,6 +3,7 @@ import { App } from 'aws-cdk-lib';
 import 'source-map-support/register';
 import { AlbStack } from '../lib/alb-stack';
 import { VpcStack } from '../lib/vpc-stack';
+import { RdsStack } from '../lib/rds-stack';
 
 const app = new App({});
 // Create the VPC stack
@@ -13,5 +14,11 @@ const vpcStack = new VpcStack(app, 'MedianVPCStack', {
 // Create the ALB stack
 new AlbStack(app, 'MedianAlbStack', {
   description: 'Median ALB Stack',
+  vpc: vpcStack.vpc,
+});
+
+// Create the RDS stack
+new RdsStack(app, 'MedianRdsStack', {
+  description: 'Median RDS Stack',
   vpc: vpcStack.vpc,
 });
